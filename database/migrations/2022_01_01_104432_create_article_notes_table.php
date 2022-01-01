ab9ae4516f88e9175caa3c32e4df4398ba81c054
Tahsin Gökalp Şaan <tahsinsaan@gmaill.com>
2022-01-01 13:48:01 +0300

    Makale notları migration eklenmesi

---
+++ database/migrations/2022_01_01_104432_create_article_notes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_notes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('article_id')->unsigned();
+            $table->text('note');
+            $table->timestamps();
+            $table->foreign('article_id')->references('id')->on('articles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article_notes');
+    }
+}
